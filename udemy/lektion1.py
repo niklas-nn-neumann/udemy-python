1f30954ff70230f77157c27932d0bd559f692bb8
Niklas Neumann <git.shortlist341@passmail.net>
2025-07-02 06:54:31 +0200

    Lektion 1 code

---
+++ udemy/lektion1.py
@@ -0,0 +1,18 @@
+if __name__ == "__main__":
+    print("hello world")
+    a = 5 + 6
+    print (a)
+    print(f"a = {a}")
+    print(a * a)
+    print(f"a * a = {a * a}")
+    b = 2
+    print(b + a * a)
+    print((a + a) * b)
+    print(a + (a * b))
+    antwort = "ein spiel"
+    print("ich baue: " + str(antwort))
+    print(a + a + a + a + a + a)
+    familie = ["marc", "geli", "niklas"]
+    familie.append("jaron")
+    print(familie)
+    pass
